6a2306739b27b24e75a6ba7fc5bb4a28ad3c8ee9
Jalil F <jrab227@gmail.com>
2014-02-25 15:56:58 -0500

    Created chapter 1-1 page describing types, attributes, rankings, and mappings. fixes #8, fixes #9, fixes #10, fixes #2.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,55 @@ angular.module('myApp.controllers', []).
   controller('startCtrl', [function() {
 
   }])
-  .controller('chapter1Ctrl', [function(){
+  .controller('chapter1Ctrl', ['$scope', function($scope){
+
+    $scope.datatypes = [
+
+      {"name":"Nominal", "encodes":"classes", "operations":"equality"},
+      {"name":"Ordinal", "encodes":"rankings", "operations":"equality, inequality"},
+      {"name":"Quantitative - Interval", "encodes":"arbitrary zero quantities", "operations":"equality, inequality, metric (difference)"},
+      {"name":"Quantitative - Ratio", "encodes":"classes", "operations":"equality, inequality, metric (difference), ratio"},
+
+
+    ];
+
+    $scope.visAttrs = [
+      {"name":"Position", "rank":1},
+      {"name":"Length", "rank":2},
+      {"name":"Angle", "rank":3},
+      {"name":"Slope", "rank":4},
+      {"name":"Area", "rank":5},
+      {"name":"Volume", "rank":6},
+      {"name":"Color", "rank":7},
+      {"name":"Texture", "rank":8}
+    ];
+    $scope.relations = [
+
+      {"type": "Nominal", "attribute":"Length", "color":"green" },
+      {"type": "Nominal", "attribute":"Position", "color":"green" },
+      {"type": "Nominal", "attribute":"Angle", "color":"green" },
+      {"type": "Nominal", "attribute":"Slope", "color":"green" },
+      {"type": "Nominal", "attribute":"Area", "color":"green" },
+      {"type": "Nominal", "attribute":"Volume", "color":"green" },
+      {"type": "Nominal", "attribute":"Color", "color":"green" },
+      {"type": "Nominal", "attribute":"Texture", "color":"green" },
+      {"type": "Ordinal", "attribute":"Length", "color":"green" },
+      {"type": "Ordinal", "attribute":"Position", "color":"green" },
+      {"type": "Ordinal", "attribute":"Area", "color":"green" },
+      {"type": "Ordinal", "attribute":"Volume", "color":"green" },
+      {"type": "Ordinal", "attribute":"Texture", "color":"green" },
+      {"type": "Quantitative - Interval", 
+       "attribute":"Length",
+       "color":"green"
+      },
+      {"type": "Quantitative - Interval", "attribute":"Position", "color":"green" },
+      {"type": "Quantitative - Interval", "attribute":"Area", "color":"red" },
+      {"type": "Quantitative - Interval", "attribute":"Volume", "color":"red"},
+      {"type": "Quantitative - Interval", "attribute":"Color", "color":"yellow" },
+      {"type": "Quantitative - Interval", "attribute":"Texture", "color": "red" },
+
+
+    ]
 
   }]);
 
--- app/js/directives.js
+++ app/js/directives.js
@@ -24,17 +24,255 @@ angular.module('myApp.directives', []).
          var window = d3.select("chapter1")
 
          var width = 960,
-         height = 500;
+         height = 450,
+         padding = 100,
+         tableYGutter = 100,
+         tableRange = 500;
 
          var svg = window.append("svg")
            .attr("width", width)
            .attr("height", height);
 
-         var datatypes = scope.datatypes;
-         var visualattrs = scope.visualattrs;
+         var titles = svg.append("g").selectAll("g")
+            .data(scope.datatypes)
+            .enter()
+            .append("g");
 
+         var attributes = svg.append("g").selectAll("g")
+            .data(scope.visAttrs)
+            .enter()
+            .append("g")
+
+         var clicked = false;
+
+         var xScale = d3.scale.ordinal();
+
+         var yScale = d3.scale.ordinal()
+           .domain(scope.datatypes.map(function(d){
+             return d.name;
+           }))
+           .rangePoints([padding, height-50]);
+
+         var button = svg.append("text")
+            .attr({ 
+                   "x": width-60,
+                   "y": height -40
+            })
+            .style({
+               "font-family":"inherit",
+               "font-size": 24,
+               "fill":"green"
+            })
+            .text("Next")
+              .on("click", function(){
+                 if (!clicked){
+                   clicked = true
+                   removeEncodings();
+                   flipDataTypeAxis();
+                   updateDatatypes(xScale, function(foo){return 30});
+                   drawAttributes(function(foo){return padding}, yScale);
+                 }
+              });
+
+         function drawEncodings() {
+            
+            titles
+              .append("text")
+                .attr("class", "info")
+                .style({"font-family":"inherit",
+                    "font-size":"18",
+                    "fill":"grey"
+                })
+                .attr("y", function(d, i){
+                  return yScale(d.name) + 24;
+                })
+                .attr("x", function(d, i){
+                  return 40;
+                })
+                .text(function(d){
+                  return d.encodes + " " + d.operations;
+                });
+              
+         }
+
+         function removeEncodings(){
+
+           titles.selectAll("text.info")
+             .transition()
+               .delay(function(d, i){ return i * 1000 })
+               .duration(1000)
+               .attr("x", -width)
+               .each("end", function(){
+
+                 d3.select(this).remove();
+
+               });
+
+         }
+
+         function drawLine() {
          
+           var axis = svg.append("g").attr("class", "axis")
 
+           var bottomtext = axis.append("text")
+               .attr({ "x": 5,
+                       "y": 430})
+               .text("Less Accurate")
+               .style("fill", "white")
+
+           var lineData = [{'x': padding/2, 'y': 400}, 
+                           {'x': padding/2, 'y':80}] 
+          
+           var triangle = axis.append("path")
+                .attr("transform", function(){
+                  return "translate(" + padding/2 + "," + 400 + ")"
+                })
+                .attr("d", d3.svg.symbol().type("triangle-down"))
+                .attr("stroke", "white")
+                .attr("stroke-width", 2)
+                .attr("fill", "none");
+                 
+           var lineFunction = d3.svg.line()
+               .x(function(d){return d.x})
+               .y(function(d){return d.y})
+
+           var lineGraph = axis.append("path")
+               .attr("d", lineFunction(lineData))
+               .attr("stroke", "white")
+               .attr("stroke-width", 2)
+               .attr("fill", "none");
+
+           lineGraph
+                .transition(1000)
+                .delay(500)
+                .attr("stroke", "blue")
+           
+           triangle
+                .transition(1000)
+                .delay(500)
+                .attr("stroke", "blue")
+                .attr("fill", "blue")
+
+           bottomtext
+                .transition(1000)
+                .delay(500)
+                .style("fill", "blue")
+                .each("end", function(){ drawRelations() })
+
+         }
+
+         function drawAttributes(xs, ys) {
+            
+            attributes.append("text")
+              .attr("class", "attributes")
+              .style({"font-family":"inherit",
+                    "font-size":"24",
+                    "fill":"black"
+              })
+              .attr("x", function(d, i){
+                return -100;
+              })
+              .attr("y", function(d, i){
+                return ys(d.name);
+              })
+              .text(function(d){
+                return d.name;
+              });
+
+            d3.selectAll("text.attributes")
+              .transition()
+              .duration(1000)
+              .delay(1000)
+              .attr("x", function(d, i){
+                return xs(d.name);
+              })
+              .each("end", function(){
+                drawLine()
+              })
+           
+         };
+
+         function drawRelations(){
+
+           var relations = svg.append("g").attr("class", "relations")
+           
+           var circles = relations.selectAll("circle").data(scope.relations).enter().append("circle")
+             .attr({"cx": function(d){ return xScale(d.type)+ 35},
+                   "cy": function(d){ return yScale(d.attribute) - 5},
+                   "r" : 0,
+                   "fill": function(d){ return d.color} 
+             });
+
+           relations.selectAll("circle")
+              .transition()
+                .duration(1000).attr("r", 10)
+ 
+         };
+
+         function drawDatatypes(xs, ys) {
+            
+            titles.append("text")
+              .attr("class", "types")
+              .style({"font-family":"inherit",
+                    "font-size":"24",
+                    "fill":"black"
+              })
+              .attr("x", function(d, i){
+                return xs(d.name);
+              })
+              .attr("y", function(d, i){
+                return ys(d.name);
+              })
+              .text(function(d){
+                return d.name;
+              });
+
+            
+         };
+
+         function updateDatatypes(xs, ys){
+           
+            d3.selectAll("text.types")
+              .transition()
+                .delay(1000)
+                .duration(1000)
+              .attr("x", function(d, i){
+                return xs(d.name);
+              })
+              .attr("y", function(d, i){
+                return ys(d.name);
+              })
+              .text(function(d){
+                var output = d.name;
+                if (d.name.indexOf("Interval") != -1){
+                  output = "Quantitative";
+                } else if(d.name.indexOf("Ratio") != -1) {
+                  output = "";
+                }; 
+                return output;
+              });
+
+         }
+
+         function flipDataTypeAxis() {
+
+           xScale
+             .domain(scope.datatypes.map(function(d){
+               return d.name;
+             }))
+             .rangePoints([
+                  padding + tableYGutter , 
+                  padding + tableYGutter + tableRange]);
+
+           yScale
+             .domain(scope.visAttrs.map(function(d){return d.name}))
+             .rangePoints([80, 400]) 
+         };
+
+         //Initial Load
+
+         drawDatatypes(function(foo){return 30}, yScale);   
+         drawEncodings()
       }
     };
   }]).
@@ -45,12 +283,12 @@ angular.module('myApp.directives', []).
          var window = d3.select("network")
 
          var width = 960,
-         height = 500;
+         height = 400;
 
          var color = d3.scale.category20();
 
          var force = d3.layout.force()
-           .charge(-120)
+           .charge(-80)
            .linkDistance(100)
            .size([width, height]);
 
